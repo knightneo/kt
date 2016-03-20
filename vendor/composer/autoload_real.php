c8c394bc15dbd9716f64135c7dc73e65685f6c11
knightneo <knightneogarden@gmail.com>
2016-03-20 21:00:26 +0800

    finish tymon/jwt-auth configuration

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,6 +23,10 @@ class ComposerAutoloaderInit0dd3d4fd9c58f304ab6707c594d89c67
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit0dd3d4fd9c58f304ab6707c594d89c67', 'loadClassLoader'));
 
+        $includePaths = require __DIR__ . '/include_paths.php';
+        array_push($includePaths, get_include_path());
+        set_include_path(join(PATH_SEPARATOR, $includePaths));
+
         $map = require __DIR__ . '/autoload_namespaces.php';
         foreach ($map as $namespace => $path) {
             $loader->set($namespace, $path);
